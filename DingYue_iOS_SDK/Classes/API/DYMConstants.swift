2f4634eab031be5f810d7a764451883fd848956a
DingyueIO <support@dingyue.io>
2022-07-25 15:59:07 +0800

    对下载的zip包中缺失config.js文件的处理/对未完成的内购订单的结束

--- DingYue_iOS_SDK/Classes/API/DYMConstants.swift
+++ DingYue_iOS_SDK/Classes/API/DYMConstants.swift
@@ -29,7 +29,7 @@ class DYMConstants: NSObject {
     }
 
     enum Versions {
-        static let SDKVersion = "0.1.8"
+        static let SDKVersion = "0.1.9"
         static let SDKBuild = 1
     }
     enum BundleKeys {
